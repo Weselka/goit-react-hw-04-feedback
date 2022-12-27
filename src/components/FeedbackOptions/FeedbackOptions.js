778eecbd306e95e5b37d7b7c7133172064321fef
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-12-27 21:39:09 +0200

    remove comments

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -27,28 +27,3 @@ FeedbackOptions.propTypes = {
   options: PropTypes.objectOf(PropTypes.number).isRequired,
   onLeaveFeedback: PropTypes.func.isRequired,
 };
-
-// import PropTypes from 'prop-types';
-
-// import { ControlsBox, Button } from './FeedbackOptions.styled';
-
-// export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-//   return (
-//     <ControlsBox>
-//       {options.map(optionState => {
-//         return (
-//           <li key={optionState}>
-//             <Button onClick={() => onLeaveFeedback(optionState)}>
-//               {optionState}
-//             </Button>
-//           </li>
-//         );
-//       })}
-//     </ControlsBox>
-//   );
-// };
-
-// FeedbackOptions.propTypes = {
-//   options: PropTypes.array.isRequired,
-//   onLeaveFeedback: PropTypes.func.isRequired,
-// };
