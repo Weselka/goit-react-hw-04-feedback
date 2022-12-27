68b0503d31dad41fb594527e5515668abd79c624
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-12-27 21:33:29 +0200

    refactoring FeedbackOptions
    transferring the Object.keys() method from App to FeedbackOptions

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -3,12 +3,17 @@ import PropTypes from 'prop-types';
 import { ControlsBox, Button } from './FeedbackOptions.styled';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+
+  const onClickBtn = optionState => {
+    onLeaveFeedback(optionState);
+  };
+
   return (
     <ControlsBox>
-      {options.map(optionState => {
+      {Object.keys(options).map(optionState => {
         return (
           <li key={optionState}>
-            <Button onClick={() => onLeaveFeedback(optionState)}>
+            <Button onClick={() => onClickBtn(optionState)}>
               {optionState}
             </Button>
           </li>
@@ -19,6 +24,31 @@ export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
 };
 
 FeedbackOptions.propTypes = {
-  options: PropTypes.array.isRequired,
+  options: PropTypes.objectOf(PropTypes.number).isRequired,
   onLeaveFeedback: PropTypes.func.isRequired,
 };
+
+// import PropTypes from 'prop-types';
+
+// import { ControlsBox, Button } from './FeedbackOptions.styled';
+
+// export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+//   return (
+//     <ControlsBox>
+//       {options.map(optionState => {
+//         return (
+//           <li key={optionState}>
+//             <Button onClick={() => onLeaveFeedback(optionState)}>
+//               {optionState}
+//             </Button>
+//           </li>
+//         );
+//       })}
+//     </ControlsBox>
+//   );
+// };
+
+// FeedbackOptions.propTypes = {
+//   options: PropTypes.array.isRequired,
+//   onLeaveFeedback: PropTypes.func.isRequired,
+// };
